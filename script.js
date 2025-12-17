2d555f8295c5f7c18055e1e3238d5a99b059fa30
Noah Gullo <noahgulloingram@gmail.com>
2025-12-17 10:35:14 -0800

    Add operate() to call any arithmetic function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum = 0;
+let operation = "";
+let secondNum = 0;
+
 function add(a, b){
     return a + b;
 }
@@ -12,4 +16,24 @@ function multiply(a, b){
 
 function divide(a, b){
     return a / b;
+}
+
+function operate(operator, num1, num2){
+    let result = 0;
+    switch (operator){
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+    }
+
+    return result;
 }
